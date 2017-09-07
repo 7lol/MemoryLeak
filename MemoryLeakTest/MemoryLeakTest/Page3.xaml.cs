745b8d5fb247af9f1f0d68bf1f7b351be32fa3a9
Filip Toruń <filip.torun@datarino.com>
2017-09-07 17:26:47 +0200

    few changes

---
+++ MemoryLeakTest/MemoryLeakTest/Page3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MemoryLeakTest {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage {
+        ContentPage page;
+        public Page3(ContentPage page) {
+            InitializeComponent();
+            this.page = page;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
+            Navigation.PushAsync(page);
+        }
+
+        private void Button_Clicked2(object sender, EventArgs e) {
+            FFImageLoading.ImageService.Instance.InvalidateMemoryCache();
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+        }
+    }
+}
